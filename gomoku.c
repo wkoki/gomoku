df11af1f3c429deecfe9873b55dad997d88db346
Koki Wataoka <kokikiechiri@icloud.com>
2017-06-13 19:16:39 +0900

    foundation

--- gomoku.c
+++ gomoku.c
@@ -17,18 +17,22 @@
 
 // ----------Global Variables---------- //
 int board[BOARD_SIZE][BOARD_SIZE];
-
+int selected_row, selected_col;
 
 
 // -----------Prototype Decleration---------- //
-void game(void);
 void init(void);
+void game(void);
 char _show(int);
 void show(void);
+int judge(int, int, int);
+int judge_row(int, int, int);
+int judge_col(int, int, int);
+int judge_right_slant(int, int, int);
+int judge_left_slant(int, int, int);
 void put(int, int);
 void human_put(int);
-int judge(void);
-
+void ai_put(int);
 
 // ----------Main---------- //
 int main(int argc, char *argv[]){
@@ -49,12 +53,21 @@ void init(void) {
 
 void game(void) {
   printf("ゲームスタート\n");
-  do{
-    show();
+  show();
+  while(True){
     put(Player1, o);
+    if(judge(selected_row, selected_col, o)){
+      printf("Player1の勝利\n");
+      break;
+    }
     show();
     put(Player2, x);
-  }while(judge());
+    if(judge(selected_row, selected_col, x)){
+      printf("Player2の勝利\n");
+      break;
+    }
+    show();
+  }
 }
 
 char _show(int ox){
@@ -85,14 +98,135 @@ void show(void) {
   }printf("+\n");
 }
 
-
 int judge(int row, int col, int ox) {
+  if(judge_row(row, col, ox)){
+    return True;
+  }
+  if(judge_col(row, col, ox)){
+    return True;
+  }
+  if(judge_right_slant(row, col, ox)){
+    return True;
+  }
+  if(judge_left_slant(row, col, ox)){
+    return True;
+  }
+
+  return False;
+}
+
+int judge_row(int row, int col, int ox) {
   int liner = 0;
+
+  for(int i = 1; i < 5; i++){
+    if(board[row][col] == board[row+i][col]) {
+      liner++;
+    } else {
+      break;
+    }
+  }
+
+  for(int i = 1; i < 5; i++) {
+    if(board[row][col] == board[row-i][col]){
+      liner++;
+    } else {
+      break;
+    }
+  }
+
+  if(liner >= 4) {
+    return True;
+  } else {
+    return False;
+  }
 
 }
 
-void put(int player, int ox) {
 
+
+int judge_col(int row, int col, int ox) {
+  int liner = 0;
+
+  for(int i = 1; i < 5; i++){
+    if(board[row][col] == board[row][col+i]) {
+      liner++;
+    } else {
+      break;
+    }
+  }
+
+  for(int i = 1; i < 5; i++) {
+    if(board[row][col] == board[row][col-i]){
+      liner++;
+    } else {
+      break;
+    }
+  }
+
+  if(liner >= 4) {
+    return True;
+  } else {
+    return False;
+  }
+}
+
+int judge_right_slant(int row, int col, int ox) {
+  int liner = 0;
+
+  for(int i = 1; i < 5; i++){
+    if(board[row][col] == board[row+i][col+i]) {
+      liner++;
+    } else {
+      break;
+    }
+  }
+
+  for(int i = 1; i < 5; i++) {
+    if(board[row][col] == board[row-i][col-i]){
+      liner++;
+    } else {
+      break;
+    }
+  }
+
+  if(liner >= 4) {
+    return True;
+  } else {
+    return False;
+  }
+}
+
+
+
+int judge_left_slant(int row, int col, int ox) {
+  int liner = 0;
+
+  for(int i = 1; i < 5; i++){
+    if(board[row][col] == board[row+i][col-i]) {
+      liner++;
+    } else {
+      break;
+    }
+  }
+
+  for(int i = 1; i < 5; i++) {
+    if(board[row][col] == board[row-i][col+i]){
+      liner++;
+    } else {
+      break;
+    }
+  }
+
+  if(liner >= 4) {
+    return True;
+  } else {
+    return False;
+  }
+}
+
+
+
+void put(int player, int ox) {
   switch(player){
   case Human:
     human_put(ox);
@@ -104,12 +238,11 @@ void put(int player, int ox) {
 }
 
 void human_put(int ox) {
-  int row, col;
   printf("横列：");
-  scanf("%d", &row);
+  scanf("%d", &selected_row);
   printf("縦列：");
-  scanf("%d", &col);
-  board[row][col] = ox;
+  scanf("%d", &selected_col);
+  board[selected_row][selected_col] = ox;
 }
 
 
